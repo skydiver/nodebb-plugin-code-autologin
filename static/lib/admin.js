4fdfdd49717242846e9cdd9d5977357aeb85e987
Martín M <skydiver@users.noreply.github.com>
2021-03-18 20:46:51 -0300

    cleanup admin settings store function

--- static/lib/admin.js
+++ static/lib/admin.js
@@ -2,27 +2,27 @@
 
 /* globals $, app, socket, define, config */
 
-define('admin/plugins/code-autologin', ['settings', 'uploader', 'admin/modules/colorpicker'], function (settings, uploader, colorpicker) {
-	var ACP = {};
+define('admin/plugins/code-autologin', ['settings'], function (settings) {
+  var ACP = {};
 
-	ACP.init = function () {
-		settings.load('code-autologin', $('.code-autologin-settings'));
-		$('#save').on('click', saveSettings);
-	};
+  ACP.init = function () {
+    settings.load('code-autologin', $('.code-autologin-settings'));
+    $('#save').on('click', saveSettings);
+  };
 
-	function saveSettings() {
-		settings.save('code-autologin', $('.code-autologin-settings'), function () {
-			app.alert({
-				type: 'success',
-				alert_id: 'code-autologin-saved',
-				title: 'Settings Saved',
-				message: 'Please reload your NodeBB to apply these settings',
-				clickfn: function () {
-					socket.emit('admin.reload');
-				},
-			});
-		});
-	}
+  function saveSettings() {
+    settings.save('code-autologin', $('.code-autologin-settings'), function () {
+      app.alert({
+        type: 'success',
+        alert_id: 'code-autologin-saved',
+        title: 'Settings Saved',
+        message: 'Please reload your NodeBB to apply these settings',
+        clickfn: function () {
+          socket.emit('admin.reload');
+        },
+      });
+    });
+  }
 
-	return ACP;
+  return ACP;
 });
