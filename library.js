c95a9db77a116f4c870cd854003eaaf7da972444
Martín M <skydiver@users.noreply.github.com>
2021-03-18 20:54:04 -0300

    use settings from admin, code cleanup

--- library.js
+++ library.js
@@ -2,95 +2,74 @@
 
 const axios = require('axios');
 
-const nconf = require.main.require('nconf');
 const winston = require.main.require('winston');
-const controllers = require('./lib/controllers');
 const user = require.main.require('./src/user');
+const meta = require.main.require('./src/meta');
+const controllers = require('./lib/controllers');
 const authentication = require.main.require('./src/controllers/authentication.js');
 
 const plugin = {};
+let settings;
 
 plugin.init = async (params) => {
   const { router, middleware } = params;
   const routeHelpers = require.main.require('./src/routes/helpers');
 
-  /*****************************************************************************
-   *  AUTOLOGIN ROUTE
-   *  Converts a CODE to a EMAIL by calling a custom endpoint
-   *************************************************************************** */
-  routeHelpers.setupPageRoute(router, '/autologin/:code', middleware, [async (req, res, next) => {
-    const { code } = req.params;
+  /** GET PLUGIN SETTINGS */
+  settings = await meta.settings.get('code-autologin')
 
-    const email = 'USE CUSTOM EMAIL';
-
-    const { data } = await axios.get('https://jsonplaceholder.typicode.com/posts/1');
-    console.log(data);
-
-    user.getUidByEmail(email, function (err, uid) {
-      if (!uid) {
-        return res.sendStatus(404);
-      }
-
-      winston.info('Secretly logging uid ' + uid + ' for session ' + req.sessionID);
-
-      authentication.doLogin(req, uid, function (err) {
-        if (err) {
-          res.statusCode = 500;
-          res.end('Error: ' + err);
-          return winston.error('Could not log in: ' + err);
-        }
-
-        winston.info(`Successfully logged uid ${uid}, redirect to home`);
-        res.setHeader('Location', '/');
-        res.statusCode = 302;
-        res.end();
-      });
+  /** IF ENABLED, INITIALIZE AUTOLOGIN ROUTE */
+  if (settings['autologin-enabled'] === 'on') {
+    routeHelpers.setupPageRoute(router, '/autologin/:code', middleware, [autologinRoute], (req, res) => {
+      // res.sendStatus(200);
     });
+  }
 
-    setImmediate(next);
-  }], (req, res) => {
-    // res.sendStatus(200);
-  });
-
-  /*****************************************************************************
-   *  ADMIN PAGE FOR PLUGIN SETUP
-   *************************************************************************** */
+  /** ADMIN PAGE FOR PLUGIN SETUP */
   routeHelpers.setupAdminPageRoute(router, '/admin/plugins/code-autologin', middleware, [], controllers.renderAdminPage);
 };
 
-/**
- * If you wish to add routes to NodeBB's RESTful API, listen to the `static:api.routes` hook.
- * Define your routes similarly to above, and allow core to handle the response via the
- * built-in helpers.formatApiResponse() method.
- *
- * In this example route, the `authenticate` middleware is added, which means a valid login
- * session or bearer token (which you can create via ACP > Settings > API Access) needs to be
- * passed in.
- *
- * To call this example route:
- *   curl -X GET \
- * 		http://example.org/api/v3/plugins/foobar/test \
- * 		-H "Authorization: Bearer some_valid_bearer_token"
- *
- * Will yield the following response JSON:
- * 	{
- *		"status": {
- *			"code": "ok",
- *			"message": "OK"
- *		},
- *		"response": {
- *			"foobar": "test"
- *		}
- *	}
- */
-// plugin.addRoutes = async ({ router, middleware, helpers }) => {
-// 	router.get('/quickstart/:param1', middleware.authenticate, (req, res) => {
-// 		helpers.formatApiResponse(200, res, {
-// 			foobar: req.params.param1,
-// 		});
-// 	});
-// };
+/*****************************************************************************
+ *  AUTOLOGIN ROUTE
+ *  Converts a CODE to a EMAIL by calling a custom endpoint
+ *************************************************************************** */
+const autologinRoute = async (req, res, next) => {
+  const { code } = req.params;
+  const endpoint = settings['endpoint'];
 
+  /** CALL ENDPOINT WITH CODE AND EXPECT AN EXISTING EMAIL ADDRESS */
+  const { data } = await axios.get(`${endpoint}?code=${code}`);
+
+  const email = 'USE CUSTOM EMAIL';
+
+  /** TRIES TO AUTHENTICATE RETURNED EMAIL */
+  user.getUidByEmail(email, function (err, uid) {
+    if (!uid) {
+      return res.sendStatus(404);
+    }
+
+    winston.info('Secretly logging uid ' + uid + ' for session ' + req.sessionID);
+
+    authentication.doLogin(req, uid, function (err) {
+      if (err) {
+        res.statusCode = 500;
+        res.end('Error: ' + err);
+        return winston.error('Could not log in: ' + err);
+      }
+
+      winston.info(`Successfully logged uid ${uid}, redirect to home`);
+      res.setHeader('Location', '/');
+      res.statusCode = 302;
+      res.end();
+    });
+  });
+
+  setImmediate(next);
+}
+
+/*****************************************************************************
+ *  ADDS PLUGIN SETTINGS IN ADMIN DASHBOARD
+ *************************************************************************** */
 plugin.addAdminNavigation = function (header, callback) {
   header.plugins.push({
     route: '/plugins/code-autologin',
